1705997239d08574665345813c89ee2a4e3a31e3
Anna Ohanian <annaoganan92@gmail.com>
2022-10-30 19:04:31 +0300

    2

--- homework/homework2.js
+++ homework/homework2.js
@@ -1,2 +1,10 @@
-var converter = require('number-to-words');
-console.log(converter.toWords(-5))
+var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+function convert(num) {
+    if  (num == 0)
+            return 'zero'
+    else
+        return ones[num % 10]
+}
+var cases = [4,6,7]
+for (let i = 0; i < cases.length; i++)
+    console.log(Array(convert(cases[i])))
