04b2e4a1c2e984f9bea40621d57ce03086238c79
Anna Ohanian <annaoganan92@gmail.com>
2022-11-18 21:23:38 +0200

    1

---
+++ Search/LinearSearch.js
@@ -0,0 +1,18 @@
+let Arr = [3,4,10,13,33,10,16,16,16]
+function linearSearch(Arr){
+    for(let i=0; i < Arr.length; i++){
+        if(Arr[i]= 13){
+            return i
+        }
+    }
+}
+
+function mySearch(Arr, target){
+    for(let i=0; i < Arr.length; i++){
+        if(Arr[i] === target){
+        return i
+     }
+}
+    return -1
+}
+console.log(linearSearch(Arr, 16))
