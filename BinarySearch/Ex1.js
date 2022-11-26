46db0b54d525a65db159aef416f48e784caf4646
Anna Ohanian <annaoganan92@gmail.com>
2022-11-26 11:06:04 +0200

    1

--- BinarySearch/Ex1.js
+++ BinarySearch/Ex1.js
@@ -1,21 +1,21 @@
-let A = [4,1,3,88,9,9,16,6,74,97,100]
- function binarySearch(A, key){
+let A = [1,3,7,9,15,23,45,56,75,86,95]
+function binarySearch(A, key){
     let i=0; 
-    let j= A.lenght -1; 
+    let j= A.length -1; 
     let indx = -1; 
-    let middle
+    let middle;
     while (i<=j) {
         middle  = Math.floor((i+j)/2);
-        if(A[middle] == key){
-            indx = middle;
-            break;
-        }
-        if(A[middle] > key){
-            j = middle - 1
-        }else{
-            i = middle + 1
-        }
+            if(A[middle] == key){
+                indx = middle;
+                break;
+            }
+            if(A[middle] > key){
+                j = middle - 1
+            }else{
+                i = middle + 1
+            }
     }
     return indx;
  }
-  console.log(binarySearch(A,22))
+console.log(binarySearch(A, 23))
---
+++ BinarySearch/Ex2.js
@@ -0,0 +1,34 @@
+let A = [4,1,3,88,9,8,16,6,74,97,100]
+
+function InsertionSort(array){
+    for (let i=0; i < array.length; i++){
+      let element = array[i];
+      let prev_index = i-1;
+       while(prev_index >=0 && array[prev_index] > element) {
+         array[prev_index + 1] = array[prev_index]; prev_index--;}
+         array[prev_index + 1] = element;
+    }
+   return A;
+  }
+
+function binarySearch(A, key){
+    InsertionSort(A)
+    let i=0; 
+    let j= A.length -1; 
+    let indx = -1; 
+    let middle;
+    while (i<=j) {
+        middle  = Math.floor((i+j)/2);
+            if(A[middle] == key){
+                indx = middle;
+                break;
+            }
+            if(A[middle] > key){
+                j = middle - 1
+            }else{
+                i = middle + 1
+            }
+    }
+    return indx;
+ }
+console.log(binarySearch(A, 9))
