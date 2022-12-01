3bd00bf48e185a6c8bc6f9ea701d317ee9fc18f9
Anna Ohanian <annaoganan92@gmail.com>
2022-12-01 11:01:57 +0200

    1

---
+++ Classes/1.js
@@ -0,0 +1,39 @@
+class Xiaomi {
+    constructor(){
+        this.weight = "135 g (4.76 oz)";
+        this.Announced = "2007, January. Released 2007, June";
+        this.color = black;
+        this.memory = "256GB";
+        this.size = "3.5 inches, 36.5 cm2 (~52.0% screen-to-body ratio)"
+        this.Resolution  = "320 x 480 pixels, 3:2 ratio (~165 ppi density)   "
+        this.MAIN_CAMERA  = "12 MP"
+        this.DISPLAy_Type = "Liquid Retina IPS LCD, 625 nits (typ)"
+    }
+}
+
+class Iphone {
+    constructor(){
+        this.weight = "173 g (6.10 oz)";
+        this.Announced = "2019, February 20";
+        this.color = Lavender_Violet;
+        this.memory = "64GB";
+        this.size = "6.39 inches, 100.2 cm2 (~85.2% screen-to-body ratio)"
+        this.Resolution  = "1080 x 2340 pixels, 19.5:9 ratio (~403 ppi density) x 480 pixels, 3:2 ratio (~165 ppi density)   "
+        this.MAIN_CAMERA  = "48 MP"
+        this.Display = "Contrast ratio: Infinite (nominal), 3.921 (sunlight)"
+    }
+}
+
+
+class Samsung {
+    constructor(){
+        this.weight = "498 g (Wi-Fi), 500 g (LTE), 502 g (5G) (1.10 lb)";
+        this.Announced = "2020, August 05";
+        this.color = Phantom_Navy;
+        this.memory = "512GB";
+        this.size = "11.0 inches, 350.9 cm2 (~83.6% screen-to-body ratio)"
+        this.Resolution  = "1600 x 2560 pixels, 16:10 ratio (~274 ppi density)"
+        this.MAIN_CAMERA  = "13 MP"
+        this.DISPLAY_Type = "TFT LCD, 120Hz, HDR10+"
+    }
+}
