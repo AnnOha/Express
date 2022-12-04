564724d21f897efb296ed12d7f04e78f6032e854
Anna Ohanian <annaoganan92@gmail.com>
2022-12-04 12:02:26 +0200

    1

--- BinarySearch/3Star.js
+++ BinarySearch/3Star.js
@@ -1,29 +1,40 @@
 let A = [1,3,7,9,15,23,45,56,75,86,95]
-function binarySearch(A, key){
-    let i=0; 
-    let j= A.length -1; 
-    let indx = -1; 
-    let middle;
-    
-    return indx;
- }
+function binarySearch(A, number) {
 
- function recursiveSearch(){
-    while (i<=j) {
-        middle  = Math.floor((i+j)/2);
-        if(A[middle] == key){
-            indx = middle;
-            break;
-        }
-        if(A[middle] > key){
-            j = middle - 1
-        }else{
-            i = middle + 1
-        }
+    function recursiveSearch(first, last) {
+      let middle = Math.floor((first + last) / 2);
+      if (first > last) {
+        return -1;
+      }
+      if (A[middle] === number) {
+        return middle;
+      }
+      if (A[middle] > number) {
+        return recursiveSearch(first, middle - 1);
+      }else{
+        return recursiveSearch(middle + 1, last);
+      }
     }
- }
+    return recursiveSearch(0, A.length - 1);
+  }
+
+console.log(binarySearch(A, 95));
 
 
 
 
-console.log(binarySearch(A, 23))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
