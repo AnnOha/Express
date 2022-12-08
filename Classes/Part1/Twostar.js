96bb6c55a75f6b59b3e1c1ab332b2290360c0fe8
Anna Ohanian <annaoganan92@gmail.com>
2022-12-08 10:25:23 +0200

    1

--- Classes/Part1/Twostar.js
+++ Classes/Part1/Twostar.js
@@ -15,11 +15,18 @@ function creatClass(weight, color, memory, Resolution, size, MAIN_CAMERA, announ
     }
    
     class Xiaomi extends Phone {
+        
     }
   
     class Samsung extends Phone {
     }
-    return new Iphone (weight, color, memory, Resolution, size, MAIN_CAMERA, announce);
+   new Iphone (weight, color, memory, Resolution, size, MAIN_CAMERA, announce);
+   console.log( new Iphone(540, "yellow", 64, 11, "1764x2500 px", 9, 2005))
+   new Xiaomi (weight, color, memory, Resolution, size, MAIN_CAMERA, announce);
+   console.log(new Xiaomi(450, "purple", 256 , "7.10 inches", "2000x2500 px", "16MP", 2002))
+   new Samsung (weight, color, memory, Resolution, size, MAIN_CAMERA, announce);
+   console.log(new Samsung(488, "navy", 125, 9, "1150x1952 px", "20MP", 2016))
    
+
 }
-console.log(creatClass(540, "yellow", 64, 11, "1764x2500 px", 9, 2005))
+ console.log(creatClass())
