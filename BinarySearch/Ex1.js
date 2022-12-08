c902c27c3a75071cfdb74bac72a8c8c5ad7af01b
Anna Ohanian <annaoganan92@gmail.com>
2022-12-08 09:56:53 +0200

    1

--- BinarySearch/Ex1.js
+++ BinarySearch/Ex1.js
@@ -1,22 +1,21 @@
-class Staff {
-    constructor (lastName){
-        this.lastName = lastName;
+let A = [1,3,7,9,15,23,45,56,75,86,95]
+function binarySearch(A, key){
+    let i=0; 
+    let j= A.length -1; 
+    let indx = -1; 
+    let middle;
+    while (i<=j) {
+        middle  = Math.floor((i+j)/2);
+            if(A[middle] == key){
+                indx = middle;
+                break;
+            }
+            if(A[middle] > key){
+                j = middle - 1
+            }else{
+                i = middle + 1
+            }
     }
-}
-
-class RemoteStaff extends Staff {
-    constructor(lastName, schedule) { 
-        super(lastName);
-        this.monday = schedule.monday;
-        this.tuesday = schedule.tuesday;
-        this.wednesday = schedule.wednesday;
-        this.thursday = schedule.thursday;
-        this.friday = schedule.friday;
-        this.saturday = schedule.saturday;
-        this.sunday = schedule.sunday;
-    }
-}
-
-const remotemanag = new RemoteStaff('Spivak', { monday: "7:00 - 16:00", tuesday: "9:00 - 17:00", wednesday: "8:00 - 10:00", thursday: "10:00 - 20:00", friday: "12:00 - 22:00", saturday: "8:00 - 18:00", sunday: "10:00 - 13:00" })
-
-console.log(remotemanag)
+    return indx;
+ }
+console.log(binarySearch(A, 23))
