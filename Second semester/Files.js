2785f30ae2edba3aab8aa68db8c566c039cc5574
Anna Ohanian <annaoganan92@gmail.com>
2023-02-19 16:24:08 +0100

    1

--- Second semester/Files.js
+++ Second semester/Files.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+//1. Завдання
 
 fs.open("Faab.txt", "r", (err, file) => {
    if (err) throw err;
@@ -7,14 +8,26 @@ fs.open("Faab.txt", "r", (err, file) => {
       console.log(data.toString());
    });
 });
-
+//2
 fs.writeFile("Acortar.txt", `Welcome to Velaris.${String.fromCodePoint(0x1F320)}`, (err) => {
     if (err) throw err;
     console.log("Created!");
  });
 
-
+//3.
  fs.appendFile("Acortar.txt", " The city of Starlight", (err) => {
    if (err) throw err;
    console.log("Added!");
 });
+
+//4
+try {
+   if(fs.existsSync('Acotar.txt')){
+      fs.unlinkSync('Acortar.txt');
+      console.log("Delete File successfully.");
+   }else{
+      console.log("Does not exist.");
+   }
+ } catch (error) {
+   console.log(error);
+ }
