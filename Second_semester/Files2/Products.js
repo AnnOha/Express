61a15eb69c9b75da52f3a147448dec3799c23d21
Anna Ohanian <annaoganan92@gmail.com>
2023-02-27 14:31:59 +0100

    1

--- Second_semester/Files2/Products.js
+++ Second_semester/Files2/Products.js
@@ -1,77 +1,66 @@
 const fs = require('fs');
-const csv = require('csv-parser');
 
-// Додавання продуктів у список (CSV файл)
-function addProduct(productName, quantity, price) {
-  const data = `${productName},${quantity},${price}\n`;
-  fs.appendFile('shopping_list.csv', data, (err) => {
-    if (err) throw err;
-    console.log('Продукт успішно доданий до списку покупок!');
+const FILENAME = 'shopping_list.csv';
+
+if (!fs.existsSync(FILENAME)) {
+  fs.writeFileSync(FILENAME, ''); // якщо файл не існує, створюємо його
+}
+
+// Додавання продукту до списку
+function addProduct(name, quantity, price) {
+  const id = Date.now().toString(); // унікальний ідентифікатор - час створення запису
+  const product = { id, name, quantity, price };
+  const line = `${id}; ${name}; ${quantity}; ${price}`;
+  fs.appendFileSync(FILENAME, line); // додаємо запис у кінець файлу
+  console.log(`Product ${name} added to the shopping list.`);
+}
+
+// Отримання продукту за його ідентифікатором
+function getProductById(id) {
+  const data = fs.readFileSync(FILENAME, 'utf8'); // читаємо вміст файлу
+  const lines = data.trim().split(',');
+  for (const line of lines) {
+    const [productId, name, quantity, price] = line.split(';');
+    if (productId === id) {
+      return { id: productId, name, quantity, price };
+    }
+  }
+  console.log(`Product with ID ${id} not found.`);
+}
+
+// Видалення продукту за його ідентифікатором
+function removeProductById(id) {
+  let data = fs.readFileSync(FILENAME, 'utf8'); // читаємо вміст файлу
+  const lines = data.trim().split('\n');
+  const newLines = lines.filter((line) => !line.startsWith(id + ',')); // відфільтровуємо записи, які треба зберегти
+  data = newLines.join('\n');
+  fs.writeFileSync(FILENAME, data.toString()); // перезаписуємо файл з новим списком продуктів
+  console.log(`Product with ID ${id} removed from the shopping list.`);
+}
+
+// Зміна продукту за його ідентифікатором
+function updateProductById(id, name, quantity, price) {
+  let data = fs.readFileSync(FILENAME, 'utf8'); // читаємо вміст файлу
+  const lines = data.trim().split('\n');
+  let isUpdated = false;
+  const newLines = lines.map((line) => {
+    const [productId, oldName, oldQuantity, oldPrice] = line.split(',');
+    if (Number(productId) === id) { // знаходимо запис за ідентифікатором
+      isUpdated = true;
+      const newLine = [`${id},${name},${quantity},${price}`];
+      console.log(`Product with ID ${id} updated in the shopping list.`);
+      return newLine;
+    }
+    return line;
   });
+  if (!isUpdated) {
+    console.log(`Product with ID ${id} not found.`);
+    return;
+  }
+  data = newLines.join('\n');
+  fs.writeFileSync(FILENAME, data); // перезаписуємо файл з новим списком продуктів
 }
-
-// Отримання даних про продукт за його унікальним індексом
-function getProduct(productId) {
-  let result = null;
-  fs.createReadStream('shopping_list.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-      if (row.id === productId) {
-        result = row;
-      }
-    })
-    .on('end', () => {
-      console.log(result);
-    });
-}
-
-// Видалення продукту зі списку (CSV файлу) за його унікальним індексом
-function deleteProduct(productId) {
-  const rows = [];
-  fs.createReadStream('shopping_list.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-      if (row.id !== productId) {
-        rows.push(row);
-      }
-    })
-    .on('end', () => {
-      const data = rows.map((row) => {
-        return `${row.id},${row.productName},${row.quantity},${row.price}\n`;
-      }).join('');
-      fs.writeFile('shopping_list.csv', data, (err) => {
-        if (err) throw err;
-        console.log('Продукт успішно видалений зі списку покупок!');
-      });
-    });
-}
-
-// Зміна продукту за його унікальним індексом
-function updateProduct(productId, newProductName, newQuantity, newPrice) {
-  const rows = [];
-  fs.createReadStream('shopping_list.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-      if (row.id === productId) {
-        row.productName = newProductName;
-        row.quantity = newQuantity;
-        row.price = newPrice;
-      }
-      rows.push(row);
-    })
-    .on('end', () => {
-      const data = rows.map((row) => {
-        return `${row.id},${row.productName},${row.quantity},${row.price}\n`;
-      }).join('');
-      fs.writeFile('shopping_list.csv', data, (err) => {
-        if (err) throw err;
-        console.log('Продукт успішно змінений в списку покупок!');
-      });
-    });
-}
-
-// Приклад використання функцій
-addProduct('Молоко', 2, 20);
-getProduct(1);
-deleteProduct(2);
-updateProduct(3, 'Хліб', 1, 15);
+addProduct("Яблуко", 3, 15)
+getProductById(2)
+removeProductById(3)
+updateProductById(2, "Нектар", 2,20)
