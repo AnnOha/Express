61018a434f39ece230cefe9d297c43ba03b6b55e
Anna Ohanian <annaoganan92@gmail.com>
2023-03-08 12:34:51 +0100

    1

--- Second_semester/Files2/Crypto.js
+++ Second_semester/Files2/Crypto.js
@@ -1,22 +1,23 @@
 const crypto = require('crypto');
 const fs = require('fs');
-function createHashedPassword(password) {
-  const salt = crypto.randomBytes(16).toString('hex'); // генеруємо випадкову сіль
-  const hash = crypto.pbkdf2Sync(password, salt, 10000, 32, 'sha512').toString('hex'); // хешуємо пароль
+
+function HashedPass(password) {
+  const salt = crypto.randomBytes(16).toString('hex'); 
+  const hash = crypto.pbkdf2Sync(password, salt, 10000, 32, 'sha512').toString('hex'); 
   return { salt, hash };
 }
 
 function hashAndSave(password, filePath) {
-  const { salt, hash } = createHashedPassword(password); // хешуємо пароль та отримуємо сіль
-  const data = JSON.stringify({ salt, hash }); // зберігаємо сіль та хеш у JSON-форматі
-  fs.writeFileSync(filePath, data); // записуємо дані у файл
+  const { salt, hash } = HashedPass(password); 
+  const data = JSON.stringify({ salt, hash }); 
+  fs.writeFileSync(filePath, data); 
   console.log('Пароль успішно збережено у файлі ' + filePath);
 }
 
 function comparePassword(password, filePath) {
-  const data = fs.readFileSync(filePath); // отримуємо дані з файлу
-  const { salt, hash } = JSON.parse(data); // розпаковуємо сіль та хеш з JSON
-  const inputHash = crypto.pbkdf2Sync(password, salt, 10000, 32, 'sha512').toString('hex'); // хешуємо введений пароль
+  const data = fs.readFileSync(filePath); 
+  const { salt, hash } = JSON.parse(data); 
+  const inputHash = crypto.pbkdf2Sync(password, salt, 10000, 32, 'sha512').toString('hex');
   const result = inputHash === hash;
   console.log(result ? 'Паролі співпадають' : 'Паролі не співпадають');
   return result;
@@ -25,22 +26,22 @@ function comparePassword(password, filePath) {
 
 
 // Отримання параметрів з командного рядка
-const args = process.argv.slice(2); // отримуємо аргументи, які передали програмі
-const command = args[0]; // отримуємо команду
+const args = process.argv.slice(2); 
+const command = args[0]; 
 
 if (command === 'hash') {
-  const password = args[1]; // отримуємо пароль
-  const filePath = args[2]; // отримуємо ім'я файлу
+  const password = args[1]; 
+  const filePath = args[2]; 
   try {
-    hashAndSave(password, filePath); // хешуємо пароль та зберігаємо у файл
+    hashAndSave(password, filePath); 
   } catch (error) {
   console.error('Сталася помилка: ', error);
   } 
   }else if (command === 'compare') {
-  const password = args[1]; // отримуємо пароль
-  const filePath = args[2]; // отримуємо ім'я файлу
+  const password = args[1];
+  const filePath = args[2]; 
   try {
-  comparePassword(password, filePath); // порівнюємо пароль з тим, що збережений у файлі
+  comparePassword(password, filePath); 
   } catch (error) {
   console.error('Сталася помилка: ', error);
   }
