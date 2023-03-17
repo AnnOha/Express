590a25a845d379bf05a4f95e254a1bcf273c964b
Anna Ohanian <annaoganan92@gmail.com>
2023-03-17 18:25:00 +0100

    1

--- Second_semester/Streams/str.js
+++ Second_semester/Streams/str.js
@@ -1,18 +1,40 @@
+
 const fileSystem = require("fs");
 var data = "";
 
-const readStream = fileSystem.createReadStream("text.txt");
+let Fletter = 0;
+let letter = false;
 
+const readStream = fileSystem.createReadStream("text.txt");
 readStream.setEncoding("UTF8");
 
 readStream.on("data", (chunk) => {
 	data += chunk;
-});
-
-readStream.on("end", () => {
 	console.log(data);
+
+	const text = chunk.toString();
+	for (let i = 0; i < text.length; i++) {
+    if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z')) {
+		letter = true;
+    } else {
+      if(letter === true) {
+        Fletter++;
+      }
+
+      letter = false;
+    }
+  }
+
+  console.log('----------------------------------------------------')
+
 });
 
-readStream.on("error", (error) => {
-	console.log(error.stack);
-});
+readStream.on('end', () => {
+  if(letter === true) {
+    Fletter++;
+  }
+
+  console.log(`'This file contains ${String.fromCodePoint(0x1F601)}:'`, Fletter, "words")
+  console.log('----------------------------------------------------')
+})
+console.log('----------------------------------------------------')
