d1bedecadb573f8158135eae4215761567ff3960
Anna Ohanian <annaoganan92@gmail.com>
2023-03-17 10:56:54 +0100

    1

--- Second_semester/Express.js/Rw.js
+++ Second_semester/Express.js/Rw.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-  res.send(`'Youre a stunning murderous little creature ${String.fromCodePoint(0x1F60F)}'`);
+  res.send(`'Youre an absolute  stunning murderous little creature ${String.fromCodePoint(0x1F60F)}'`);
+  app.get('/about', function(req, res) {
+    res.sendFile(__dirname + '/Rh.html');
+  });
+
 });
 
+  
 app.listen(8000, function() {
   console.log('Server started on port 8000');
 });
+
