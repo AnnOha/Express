af1d94c90be17b0d7e4d46b194cb67d209bad1c4
Anna Ohanian <annaoganan92@gmail.com>
2023-04-17 18:53:44 +0200

    1

---
+++ Second_semester/JestApp/test/removeZ.test.js
@@ -0,0 +1,35 @@
+const removeZ = require('../src/removeZ');
+
+describe("Z-remover Tests", () => {
+test("Remove the 'z' from words", () => {
+// arrange and act
+var result = mathOperations.sum(1,2)
+
+// assert
+expect(result).toBe(3);
+});
+
+test("Subtraction of 2 numbers", () => {
+// arrange and act
+var result = mathOperations.diff(10,2)
+
+// assert
+expect(result).toBe(8);
+});
+
+test("Multiplication of 2 numbers", () => {
+// arrange and act
+var result = mathOperations.product(2,8)
+
+// assert
+expect(result).toBe(16);
+});
+
+test("Division of 2 numbers", () => {
+// arrange and act
+var result = mathOperations.divide(24,8)
+
+// assert
+expect(result).toBe(3);
+});
+})
