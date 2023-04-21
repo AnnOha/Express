e47ab6cae4c935508d9fa41172e212049e98dc1a
Anna Ohanian <annaoganan92@gmail.com>
2023-04-21 09:34:40 +0200

    4

--- Second_semester/Project/pr.js
+++
@@ -1,7 +0,0 @@
-const abcElement = document.querySelector('#abc');
-const numElement = document.querySelector('#num123');
-const symbolsElement = document.querySelector('#symbols');
-const buttonElement = document.querySelector('#button');
-button.addEventListener('click', function(){
-alert('RHYSAND')
-})
