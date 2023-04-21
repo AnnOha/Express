d7cb5d4da0cbc778fe0c2d2f05c30e4caf353ef1
Anna Ohanian <annaoganan92@gmail.com>
2023-04-21 12:47:06 +0200

    test

--- Second_semester/JestApp/test/Lgnpass.test.js
+++ Second_semester/JestApp/test/Lgnpass.test.js
@@ -1,10 +1,28 @@
 const Lgpassw =require('../src/Lgnpass.js')
 describe("Bookboyfriend Tests", () => {
 
-test("returns secret phrase", () => {
-    expect(Lgpassw("Aaron Warner", "123")).toBe(" LYHFML");
-  });
+  test('Should return Dantes pharase"', ()=>{
+    const login = 'Dante Russo';
+    const passord=`"${String.fromCodePoint(0x1F60F)}"`;
+    expect(Lgpassw(login, passord)).toBe("CUYMV")
 })
-/*let b= [login = "Aaron Warner" , password ="123"]
-let a=  'LYHFML'
- expect(b).toBe(a);*/
+
+    test('Should return "Incorrect login or password" for incorrect login or password', () => {
+      expect(Lgpassw('Aaron Warner', 'Ellie')).toBe('Incorrect login or password');
+      
+    });
+    test('Should return Rhys  Larsens  pharase"', ()=>{
+      expect(Lgpassw('Rhys Larsen', `"${String.fromCodePoint(0x1F601)}"` )).toBe("STFUATTLDLAGG");
+    })
+
+    test('Should return Cassians pharase"', ()=>{
+        const login = 'Cassian';
+        const passord=`"${String.fromCodePoint(0x1F60E)}"`;
+        expect(Lgpassw(login, passord)).toBe("PYHOTH")
+    })
+});
+      
+
+
+
+
